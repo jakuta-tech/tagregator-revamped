0efc0c78a942c508ff36312e8ee58768758a5709
Xenos Konstantinos <mr.xkon@gmail.com>
2017-08-07 09:03:49 +0300

    instagram api done
    instagram api done

--- tagregator-revamped/functions/instagram-posts.php
+++ tagregator-revamped/functions/instagram-posts.php
@@ -5,50 +5,65 @@
 
 function tagregator_revamped_get_instagram_posts( $the_term ) {
 	$tr_options = get_option( 'tagregator_revamped_options' );
-	if ( empty( $tr_options['tagregator_revamped_instagram_access_token'] ) ||
-		empty( $tr_options['tagregator_revamped_instagram_client_id'] ) ) {
-		return;
-	}
-
-	if ( 'no' == $tr_options['tagregator_revamped_instagram_sandbox'] ) {
-		// url for PUBLIC tags // https://api.instagram.com/v1/tags/XXXX/media/recent/?access_token=XXXX
-		$url = TAGREGATOR_REVAMPED_INSTAGRAM_API . 'v1/tags/' . $the_term . '/media/recent?access_token=' . $tr_options['tagregator_revamped_instagram_access_token'];
-	} else {
-		// url for SELF posts https://api.instagram.com/v1/users/self/media/recent/?access_token=XXXX
-		$url = TAGREGATOR_REVAMPED_INSTAGRAM_API . 'v1/users/self/media/recent?access_token=' . $tr_options['tagregator_revamped_instagram_access_token'];
-	}
-
-	$response = wp_remote_get( $url );
-	$the_result = wp_remote_retrieve_body( $response );
-	$body = json_decode( $the_result, true );
-	$tag_user = get_user_by( 'login', 'TagregatorRevamped' );
-
-	foreach ( $body['data'] as $insta_post ) {
-
-		// Create post object
-		$my_post = array(
-			'post_title' => $insta_post['id'],
-			'post_content' => $insta_post['caption']['text'],
-			'post_status' => 'publish',
-			'post_author' => $tag_user->ID,
-			'post_type' => 'tagrev-instagram',
-			'meta_input' => array(
-				'tagrev_meta_id' => $insta_post['id'],
-				'tagrev_meta_name' => $insta_post['user']['full_name'],
-				'tagrev_meta_username' => $insta_post['user']['username'],
-				'tagrev_meta_profile_url' => 'https://instagram.com/' . $insta_post['user']['username'],
-				'tagrev_meta_img' => $insta_post['images']['standard_resolution']['url'],
-				'tagrev_meta_url' => $insta_post['link'],
-			),
-		);
-
-		// Insert the post into the database
-		$my_post_id = wp_insert_post( $my_post );
-
-		// Add term to post
-		if ( $my_post_id ) {
-			wp_set_object_terms( $my_post_id, $the_term, 'tagrev-hashtags', true );
+	if ( 'yes' == $tr_options['tagregator_revamped_instagram_enabled'] ) {
+		if ( empty( $tr_options['tagregator_revamped_instagram_access_token'] ) ) {
+			return;
 		}
-	}// end foreach ( $body['data'] as $insta_post )
 
+		if ( 'no' == $tr_options['tagregator_revamped_instagram_sandbox'] ) {
+			// url for PUBLIC tags // https://api.instagram.com/v1/tags/XXXX/media/recent/?access_token=XXXX
+			$url = TAGREGATOR_REVAMPED_INSTAGRAM_API . 'v1/tags/' . $the_term . '/media/recent?access_token=' . $tr_options['tagregator_revamped_instagram_access_token'];
+		} else {
+			// url for SELF posts https://api.instagram.com/v1/users/self/media/recent/?access_token=XXXX
+			$url = TAGREGATOR_REVAMPED_INSTAGRAM_API . 'v1/users/self/media/recent?access_token=' . $tr_options['tagregator_revamped_instagram_access_token'];
+		}
+
+		$response = wp_remote_get( $url );
+		$the_result = wp_remote_retrieve_body( $response );
+		$body = json_decode( $the_result, true );
+		$tag_user = get_user_by( 'login', 'TagregatorRevamped' );
+
+		foreach ( $body['data'] as $insta_post ) {
+			$post_date = date( 'Y-m-d H:i:s', $insta_post['created_time'] );
+			// Create post object
+			$my_post = array(
+				'post_title' => $insta_post['caption']['text'],
+				'post_content' => $insta_post['caption']['text'],
+				'post_date' => $post_date,
+				'post_status' => 'publish',
+				'post_author' => $tag_user->ID,
+				'post_type' => 'tagrev-instagram',
+				'meta_input' => array(
+					'tagrev_meta_id' => $insta_post['id'],
+					'tagrev_meta_name' => $insta_post['user']['full_name'],
+					'tagrev_meta_username' => $insta_post['user']['username'],
+					'tagrev_meta_profile_url' => 'https://instagram.com/' . $insta_post['user']['username'],
+					'tagrev_meta_img' => $insta_post['images']['standard_resolution']['url'],
+					'tagrev_meta_url' => $insta_post['link'],
+				),
+			);
+
+			// Check if post exists in database
+			$find_post = array(
+				'post_type' => 'tagrev-instagram',
+				'meta_query' => array(
+					array(
+						'key' => 'tagrev_meta_id',
+						'value' => $insta_post['id'],
+					),
+				),
+			);
+
+			$post_exists = new WP_Query( $find_post );
+
+			if ( ! $post_exists->have_posts() ) {
+				// Insert the post into the database
+				$my_post_id = wp_insert_post( $my_post );
+				// Add term to post
+				if ( $my_post_id ) {
+					wp_set_object_terms( $my_post_id, $the_term, 'tagrev-hashtags', true );
+				}
+			}
+		}// end foreach ( $body['data'] as $insta_post )
+	}
 }// end tagregator_revamped_get_instagram_posts
--- tagregator-revamped/settings/options.php
+++ tagregator-revamped/settings/options.php
@@ -31,6 +31,13 @@ function tagregator_revamped_options() {
 		'tagregator_revamped_twitter',
 		'tagregator_revamped_twitter'
 	);
+	add_settings_field(
+		'tagregator_revamped_twitter_enabled',
+		'Enable',
+		'tagregator_revamped_twitter_enabled',
+		'tagregator_revamped_twitter',
+		'tagregator_revamped_twitter'
+	);
 
 	// instagram settings
 	add_settings_section(
@@ -74,6 +81,13 @@ function tagregator_revamped_options() {
 		'tagregator_revamped_instagram',
 		'tagregator_revamped_instagram'
 	);
+	add_settings_field(
+		'tagregator_revamped_instagram_enabled',
+		'Enable',
+		'tagregator_revamped_instagram_enabled',
+		'tagregator_revamped_instagram',
+		'tagregator_revamped_instagram'
+	);
 
 	// google+ settings
 	add_settings_section(
@@ -89,6 +103,13 @@ function tagregator_revamped_options() {
 		'tagregator_revamped_googleplus',
 		'tagregator_revamped_googleplus'
 	);
+	add_settings_field(
+		'tagregator_revamped_googleplus_enabled',
+		'Enable',
+		'tagregator_revamped_googleplus_enabled',
+		'tagregator_revamped_googleplus',
+		'tagregator_revamped_googleplus'
+	);
 
 	// flickr settings
 	add_settings_section(
@@ -101,6 +122,13 @@ function tagregator_revamped_options() {
 		'tagregator_revamped_flickr_key',
 		'Flickr Key',
 		'tagregator_revamped_flickr_key',
+		'tagregator_revamped_flickr',
+		'tagregator_revamped_flickr'
+	);
+	add_settings_field(
+		'tagregator_revamped_flickr_enabled',
+		'Enable',
+		'tagregator_revamped_flickr_enabled',
 		'tagregator_revamped_flickr',
 		'tagregator_revamped_flickr'
 	);
@@ -125,6 +153,24 @@ function tagregator_revamped_twitter_secret() {
 	$tr_options = get_option( 'tagregator_revamped_options' );
 	echo "<input id='tagregator_revamped_twitter_secret' name='tagregator_revamped_options[tagregator_revamped_twitter_secret]' size='40' type='text' value='{$tr_options['tagregator_revamped_twitter_secret']}' />";
 }// end tagregator_revamped_twitter_secret
+
+function tagregator_revamped_twitter_enabled() {
+	$tr_options = get_option( 'tagregator_revamped_options' );
+	if ( empty( $tr_options['tagregator_revamped_twitter_enabled'] ) ) {
+		$no_selected = 'selected="selected"';
+		$yes_selected = '';
+	} elseif ( 'no' == $tr_options['tagregator_revamped_twitter_enabled'] ) {
+		$no_selected = 'selected="selected"';
+		$yes_selected = '';
+	} else {
+		$yes_selected = 'selected="selected"';
+		$no_selected = '';
+	}
+	echo '<select id="tagregator_revamped_twitter_enabled" name="tagregator_revamped_options[tagregator_revamped_twitter_enabled]">';
+	echo '<option value="no" ' . $no_selected . '>No</option>';
+	echo '<option value="yes" ' . $yes_selected . '>Yes</option>';
+	echo '</select>';
+}// end tagregator_revamped_twitter_enabled
 
 function tagregator_revamped_instagram_client_id() {
 	$tr_options = get_option( 'tagregator_revamped_options' );
@@ -162,14 +208,68 @@ function tagregator_revamped_instagram_sandbox() {
 	echo '<option value="yes" ' . $yes_selected . '>Yes</option>';
 	echo '<option value="no" ' . $no_selected . '>No</option>';
 	echo '</select>';
-}// end tagregator_revamped_instagram_redirect_url
+}// end tagregator_revamped_instagram_sandbox
+
+function tagregator_revamped_instagram_enabled() {
+	$tr_options = get_option( 'tagregator_revamped_options' );
+	if ( empty( $tr_options['tagregator_revamped_instagram_enabled'] ) ) {
+		$no_selected = 'selected="selected"';
+		$yes_selected = '';
+	} elseif ( 'no' == $tr_options['tagregator_revamped_instagram_enabled'] ) {
+		$no_selected = 'selected="selected"';
+		$yes_selected = '';
+	} else {
+		$yes_selected = 'selected="selected"';
+		$no_selected = '';
+	}
+	echo '<select id="tagregator_revamped_instagram_enabled" name="tagregator_revamped_options[tagregator_revamped_instagram_enabled]">';
+	echo '<option value="no" ' . $no_selected . '>No</option>';
+	echo '<option value="yes" ' . $yes_selected . '>Yes</option>';
+	echo '</select>';
+}// end tagregator_revamped_instagram_enabled
 
 function tagregator_revamped_flickr_key() {
 	$tr_options = get_option( 'tagregator_revamped_options' );
 	echo "<input id='tagregator_revamped_flickr_key' name='tagregator_revamped_options[tagregator_revamped_flickr_key]' size='40' type='text' value='{$tr_options['tagregator_revamped_flickr_key']}' />";
 }// end tagregator_revamped_flickr_key
 
+function tagregator_revamped_flickr_enabled() {
+	$tr_options = get_option( 'tagregator_revamped_options' );
+	if ( empty( $tr_options['tagregator_revamped_flickr_enabled'] ) ) {
+		$no_selected = 'selected="selected"';
+		$yes_selected = '';
+	} elseif ( 'no' == $tr_options['tagregator_revamped_flickr_enabled'] ) {
+		$no_selected = 'selected="selected"';
+		$yes_selected = '';
+	} else {
+		$yes_selected = 'selected="selected"';
+		$no_selected = '';
+	}
+	echo '<select id="tagregator_revamped_flickr_enabled" name="tagregator_revamped_options[tagregator_revamped_flickr_enabled]">';
+	echo '<option value="no" ' . $no_selected . '>No</option>';
+	echo '<option value="yes" ' . $yes_selected . '>Yes</option>';
+	echo '</select>';
+}// end tagregator_revamped_flickr_enabled
+
 function tagregator_revamped_googleplus_key() {
 	$tr_options = get_option( 'tagregator_revamped_options' );
 	echo "<input id='tagregator_revamped_googleplus_key' name='tagregator_revamped_options[tagregator_revamped_googleplus_key]' size='40' type='text' value='{$tr_options['tagregator_revamped_googleplus_key']}' />";
 }// end tagregator_revamped_googleplus_key
+
+function tagregator_revamped_googleplus_enabled() {
+	$tr_options = get_option( 'tagregator_revamped_options' );
+	if ( empty( $tr_options['tagregator_revamped_googleplus_enabled'] ) ) {
+		$no_selected = 'selected="selected"';
+		$yes_selected = '';
+	} elseif ( 'no' == $tr_options['tagregator_revamped_googleplus_enabled'] ) {
+		$no_selected = 'selected="selected"';
+		$yes_selected = '';
+	} else {
+		$yes_selected = 'selected="selected"';
+		$no_selected = '';
+	}
+	echo '<select id="tagregator_revamped_googleplus_enabled" name="tagregator_revamped_options[tagregator_revamped_googleplus_enabled]">';
+	echo '<option value="no" ' . $no_selected . '>No</option>';
+	echo '<option value="yes" ' . $yes_selected . '>Yes</option>';
+	echo '</select>';
+}// end tagregator_revamped_googleplus_enabled
